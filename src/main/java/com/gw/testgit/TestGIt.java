302c11b04190e8aff03d168dcb72356c901e81c4
guo110wei@126.com <guo110wei@126.com>
2017-11-16 15:12:09 +0800

    主干测试

--- src/main/java/com/gw/testgit/TestGIt.java
+++ src/main/java/com/gw/testgit/TestGIt.java
@@ -4,5 +4,6 @@ public class TestGIt {
 	public void test(){
 		System.out.println("git 分支111111111111111111111111");
 		System.out.println("22222222222222");
+		System.out.println("主干道git 提交");
 	}
 }
