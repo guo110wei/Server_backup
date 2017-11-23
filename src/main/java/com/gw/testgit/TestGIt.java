103afa9afb17fb65ee21b4fba08d8d3d169f4845
guo110wei@126.com <guo110wei@126.com>
2017-11-23 15:48:27 +0800

    master修改

--- src/main/java/com/gw/testgit/TestGIt.java
+++ src/main/java/com/gw/testgit/TestGIt.java
@@ -6,5 +6,6 @@ public class TestGIt {
 		System.out.println("主分支");
 		System.out.println("22222222222222");
 		System.out.println("主干道git 提交");
+		System.out.println("1111111111111111111111master 提交一次");
 	}
 }
